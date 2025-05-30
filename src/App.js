deb07326b0d05c275c964150020daee6838437a8
Lucero7777 <josemarin20041003@gmail.com>
2025-05-30 09:12:07 -0400

    toyo es un tirano

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import './App.css'; // Asegúrate de que este CSS esté enlazado
+import memeImage from './assets/imagen.jpg'; // Importa tu imagen
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hola mundo</h1>
+        {/* Aquí va la imagen del meme */}
+        <img
+          src={memeImage}
+          className="project-image" // Usaremos esta clase para los estilos
+          alt="Meme del proyecto Ybana"
+        />
+
       </header>
     </div>
   );
